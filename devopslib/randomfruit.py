675181a04f267ba986f42188fff04cac92cb5075
bspen327 <110974460+bspen327@users.noreply.github.com>
2023-10-06 19:55:39 +0000

    added formatting and make all command to run all at once!

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -1,5 +1,6 @@
 from random import choices
 
+
 def fruit():
-    fruits = ["apple","cherry","berry"]
+    fruits = ["apple", "cherry", "berry"]
     return choices(fruits)[0]
--- test_devopslib.py
+++ test_devopslib.py
@@ -1,6 +1,7 @@
 from devopslib.randomfruit import fruit
 
+
 def test_fruit():
     fruit_choice = fruit()
     # must be one of the three fruits inside the randomfruit.py file
-    assert fruit_choice in ["apple","cherry","berry"]
+    assert fruit_choice in ["apple", "cherry", "berry"]
